e1c59962f300b0100a79dad049ed81ad282ba501
Vinicius <vm.murad@gmail.com>
2020-02-13 01:41:55 -0300

    add logout

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -5,6 +5,7 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Background from '~/components/Background';
 import { updateProfileRequest } from '~/store/modules/user/actions';
+import { signOut } from '~/store/modules/auth/actions';
 
 import {
     Container,
@@ -13,6 +14,7 @@ import {
     FormInput,
     SubmitButton,
     Separator,
+    LogoutButton,
 } from './styles';
 
 export default function Profile() {
@@ -45,6 +47,10 @@ export default function Profile() {
                 confirmPassword,
             })
         );
+    }
+
+    function handleLogout() {
+        dispatch(signOut());
     }
 
     return (
@@ -113,6 +119,7 @@ export default function Profile() {
                     />
 
                     <SubmitButton onPress={handleSubmit}>Save</SubmitButton>
+                    <LogoutButton onPress={handleLogout}>Logout</LogoutButton>
                 </Form>
             </Container>
         </Background>
