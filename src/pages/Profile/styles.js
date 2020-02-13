44489e0695c3e3657bcda07dfbdd01fa6f0fb857
Vinicius <vm.murad@gmail.com>
2020-02-13 01:39:06 -0300

    add profile page

--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -1,3 +1,42 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+import Button from '~/components/Button';
+import Input from '~/components/Input';
+
+export const Container = styled.SafeAreaView`
+    flex: 1;
+`;
+
+export const Title = styled.Text`
+    font-size: 20px;
+    color: #fff;
+    font-weight: bold;
+    align-self: center;
+    margin-top: 30px;
+`;
+
+export const Separator = styled.View`
+    margin: 20px 0 30px;
+    height: 1px;
+    background: rgba(255, 255, 255, 0.2);
+`;
+
+export const Form = styled.ScrollView.attrs({
+    showsVerticalScrollIndicator: false,
+    contentContainerStyle: { padding: 30 },
+})`
+    align-self: stretch;
+`;
+
+export const FormInput = styled(Input)`
+    margin-bottom: 10px;
+`;
+
+export const SubmitButton = styled(Button)`
+    margin-top: 5px;
+`;
+
+export const LogoutButton = styled(Button)`
+    margin-top: 10px;
+    background: #f64c75;
+`;
